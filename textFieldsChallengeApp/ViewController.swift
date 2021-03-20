df64ff5811d225544d4bdacab50ffd3a20789915
Maleina Bidek <maleina@ucla.edu>
2021-03-20 10:37:27 -0400

    feat: Third text field only editible if switch on

--- textFieldsChallengeApp/ViewController.swift
+++ textFieldsChallengeApp/ViewController.swift
@@ -32,8 +32,29 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     // MARK: Text Field Delegate Methods
-
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        return true
+    }
     
-
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return self.textField3Switch.isOn
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Actions
+    @IBAction func toggleTextField3(_ sender: Any) {
+        if !(sender as! UISwitch).isOn {
+            self.textField3.resignFirstResponder()
+        }
+    }
+    
 }
 
